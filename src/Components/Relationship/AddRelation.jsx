b960e0a0f480adab50d4da8cf740c41cd8509253
Mamun <mamunurrashid.s.bd@gmail.com>
2024-02-27 20:37:23 +0600

    Realtionship page user search complited

--- src/Components/Relationship/AddRelation.jsx
+++ src/Components/Relationship/AddRelation.jsx
@@ -11,24 +11,58 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Base_Url } from '../../../public/utils';
 
+import PersonViewCard from './PersonViewCard';
+
 const AddRelation = () => {
-    const { setUser, setLoading, loading } = useContext(AuthContext)
     const [display, setDisplay] = useState([])
+    const [selectedPeople, setSelectedPeople] = useState(null)
     const [searchValue, setSearchValue] = useState("");
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [selectedRelationship, setSelectedRelationship] = useState(null);
+
+    // Function to handle selection
+    const handleSelect = (relationship) => {
+        setSelectedRelationship(relationship);
+        setDropdownOpen(!dropdownOpen);
+    };
+    const handleRest = () => {
+        setSelectedRelationship(null);
+        setSelectedPeople(null);
+    };
 
     const handleCancel = () => {
         setDisplay([])
     };
+    const handlePeopleClick = (id) => {
+
+        const specificElement = display.find(item => item.id == id);
+        setSelectedPeople(specificElement)
+        setDisplay([])
+        // console.log(specificElement);
+    };
+
+
 
     useEffect(() => {
         // SearchMembers()
         if (searchValue != "") {
             SearchMembers()
         }
+        console.log(selectedRelationship);
 
     }, [searchValue]);
 
+    useEffect(() => {
+        // SearchMembers()
+
+        console.log(loading);
+
+
+    }, [loading]);
+
     const SearchMembers = async () => {
+        setLoading(true);
         try {
             const response = await fetch(`${Base_Url}/api/member/search/?query=${searchValue}`, {
                 method: 'post',
@@ -58,9 +92,9 @@ const AddRelation = () => {
             <Nav_2></Nav_2>
             <section className=" py-1 bg-[#f1f5f9]">
                 <div className="w-full lg:w-8/12 px-4 mx-auto mt-6">
-                    {loading && (
+                    {/* {loading && (
                         <Loader></Loader>
-                    )}
+                    )} */}
                     <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-[#f1f5f9] border-0">
                         <div className="absolute flex bg-white bg-opacity-0 gap-5 px-4 py-4 mt-8 mb-4">
                             <button className="text-xl w- font-medium text-white py-2 px-6 bg-[#D72050]">Note</button>
@@ -79,47 +113,100 @@ const AddRelation = () => {
                         <div className="flex flex-col items-center mt-16">
                             {/* stat */}
                             <div className="w-full max-w-screen-xl mx-auto px-6">
+
                                 <div className="flex justify-center p-4 px-3 py-10">
+                                    {/* here */}
                                     <div className="w-full max-w-md">
-                                        <div className="bg-white shadow-md rounded-lg px-3 py-2 mb-4">
-                                            <div className="block text-gray-700 text-lg font-semibold py-2 px-2">
-                                                Search Your Related People
-                                            </div>
-                                            <div className="flex items-center bg-gray-200 rounded-md">
-                                                <div className="pl-2">
-                                                    <svg className="fill-current text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg"
-                                                        viewBox="0 0 24 24">
-                                                        <path className="heroicon-ui"
-                                                            d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
-                                                    </svg>
-                                                </div>
-                                                <input
-                                                    className="w-full rounded-md bg-gray-200 text-gray-700 leading-tight focus:outline-none py-2 px-2"
-                                                    onChange={(event) => setSearchValue(event.target.value)}
-                                                    id="search" type="text" placeholder="Search By Name, Email, Phone no" />
-                                            </div>
-                                            <div className="py-3 text-sm">
-                                                {
-                                                    display.map(user => <SearchDropDown
-                                                        key={user.id}
-                                                        user={user}
-                                                    ></SearchDropDown>)
-                                                }
-
-                                            </div>
+                                        <div className="bg-white shadow-md rounded-lg px-3 py-4 mb-4">
                                             {
-                                                display.length > 0 ? <div className="block bg-gray-200 text-sm text-right py-2 px-3 -mx-3 -mb-2 rounded-b-lg">
-                                                    <button onClick={handleCancel} className="hover:text-gray-600 text-gray-500 font-bold py-2 px-4">
-                                                        Cancel
-                                                    </button>
-                                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                                        Invite
-                                                    </button>
-                                                </div>
-                                                    : <></>
+                                                selectedPeople ? <div className=''>
+                                                    <PersonViewCard
+                                                        user={selectedPeople}
+                                                    ></PersonViewCard>
 
+                                                    <div className="dropdown">
+                                                        <div tabIndex={0} role="button" className="btn m-1 mt-8" onClick={() => {
+                                                            setDropdownOpen(false);
+                                                        }}>
+                                                            {selectedRelationship ? selectedRelationship : 'Select Relationship'}
+                                                        </div>
+                                                        {
+                                                            !dropdownOpen ? <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                                                                <li><a className='dark:text-white font-medium' onClick={() => handleSelect('Parent')}>Parent</a></li>
+                                                                <li><a className='dark:text-white font-medium' onClick={() => handleSelect('Spouse')}>Spouse</a></li>
+                                                                <li><a className='dark:text-white font-medium' onClick={() => handleSelect('Child')}>Child</a></li>
+                                                                <li><a className='dark:text-white font-medium' onClick={() => handleSelect('Sibling')}>Sibling</a></li>
+                                                                <li><a className='dark:text-white font-medium' onClick={() => handleSelect('Grandparent')}>Grandparent</a></li>
+                                                                <li><a className='dark:text-white font-medium' onClick={() => handleSelect('Grandchild')}>Grandchild</a></li>
+                                                                <li><a className='dark:text-white font-medium' onClick={() => handleSelect('Aunt')}>Aunt</a></li>
+                                                                <li><a className='dark:text-white font-medium' onClick={() => handleSelect('Uncle')}>Uncle</a></li>
+                                                                <li><a className='dark:text-white font-medium' onClick={() => handleSelect('Cousin')}>Cousin</a></li>
+                                                                <li><a className='dark:text-white font-medium' onClick={() => handleSelect('Niece')}>Niece</a></li>
+                                                                <li><a className='dark:text-white font-medium' onClick={() => handleSelect('Nephew')}>Nephew</a></li>
+                                                                <li><a className='dark:text-white font-medium' onClick={() => handleSelect('In-Law')}>In-Law</a></li>
+                                                            </ul>
+
+                                                                : <></>
+
+                                                        }
+                                                    </div>
+                                                    {
+                                                        selectedRelationship ? <>
+                                                            <div className='flex justify-between mt-6 pb-2'>
+                                                                <button onClick={handleRest} className="btn btn-error">REST</button>
+                                                                <button className="btn btn-warning">Make Connection</button>
+                                                            </div>
+                                                        </>
+                                                            :
+                                                            <></>
+                                                    }
+                                                    <div>
+
+                                                    </div>
+                                                </div>
+                                                    : <>
+                                                        <div className="block text-gray-700 text-lg font-semibold py-2 px-2">
+                                                            Search Your Related People
+                                                        </div>
+                                                        <div className="flex items-center bg-gray-200 rounded-md">
+                                                            <div className="pl-2">
+                                                                <svg className="fill-current text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg"
+                                                                    viewBox="0 0 24 24">
+                                                                    <path className="heroicon-ui"
+                                                                        d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
+                                                                </svg>
+                                                            </div>
+                                                            <input
+                                                                className="w-full rounded-md bg-gray-200 text-gray-700 leading-tight focus:outline-none py-2 px-2"
+                                                                onChange={(event) => setSearchValue(event.target.value)}
+                                                                id="search" type="text" placeholder="Search By Name, Email, Phone no" />
+                                                        </div>
+                                                        <div className="py-3 text-sm">
+                                                            {
+                                                                display.map(user => <SearchDropDown
+                                                                    key={user.id}
+                                                                    user={user}
+                                                                    handlePeopleClick={handlePeopleClick}
+                                                                    loading={loading}
+                                                                ></SearchDropDown>)
+                                                            }
+
+                                                        </div>
+                                                        {
+                                                            display.length > 0 ? <div className="block bg-gray-200 text-sm text-right py-2 px-3 -mx-3 -mb-4 rounded-b-lg">
+                                                                <button onClick={handleCancel} className="btn btn-error text-white font-bold py-2 px-4">
+                                                                    Cancel
+                                                                </button>
+
+                                                            </div>
+                                                                : <></>
+
+                                                        }
+                                                    </>
                                             }
+
                                         </div>
+                                        {/* end here */}
                                     </div>
                                 </div>
                             </div>
--- src/Components/Relationship/SearchDropDown.jsx
+++ src/Components/Relationship/SearchDropDown.jsx
@@ -1,20 +1,45 @@
 
 
-const SearchDropDown = ({ user }) => {
+const SearchDropDown = ({ user, handlePeopleClick, loading }) => {
     const { id, full_name, email, phone_no, place_of_birth, profession, date_of_birth, date_of_death, current_address, permanent_address, facebook, linkedin, instagram, is_alive, user_profile_img } = user;
-
+    const handleClicPeople = () => {
+        handlePeopleClick(id)
+    };
     return (
-        <div className="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
-            <div className="avatar">
-                <div className="w-6 rounded">
-                    <img src={user_profile_img ? user_profile_img : "/UserProfile.jpg"}
-                        alt="Tailwind-CSS-Avatar-component" />
+        <>{
+            loading ? <>
+                <div className="w-full py-4 rounded shadow-md animate-pulse bg-gray-900 mb-4">
+                    <div className="flex p-4 space-x-4 sm:px-8">
+                        <div className="flex-shrink-0 w-16 h-16 rounded-full bg-gray-700"></div>
+                        <div className="flex-1 py-2 space-y-4">
+                            <div className="w-full h-3 rounded bg-gray-700"></div>
+                            <div className="w-5/6 h-3 rounded bg-gray-700"></div>
+                        </div>
+                    </div>
+                    {/* <div className="p-4 space-y-4 sm:px-8">
+                        <div className="w-full h-4 rounded dark:bg-gray-700"></div>
+                        <div className="w-full h-4 rounded dark:bg-gray-700"></div>
+                        <div className="w-3/4 h-4 rounded dark:bg-gray-700"></div>
+                    </div> */}
                 </div>
-            </div>
 
-            <div className="flex-grow font-medium px-2">{full_name}</div>
-            <div className="text-sm font-normal text-gray-500 tracking-wide">{phone_no}</div>
-        </div>
+            </>
+
+                : <>
+                    <div onClick={handleClicPeople} className="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+                        <div className="avatar">
+                            <div className="w-6 rounded">
+                                <img src={user_profile_img ? user_profile_img : "/UserProfile.jpg"}
+                                    alt="Tailwind-CSS-Avatar-component" />
+                            </div>
+                        </div>
+
+                        <div className="flex-grow font-medium px-2">{full_name}</div>
+                        <div className="text-sm font-normal text-gray-500 tracking-wide">{phone_no}</div>
+                    </div>
+                </>
+        }
+        </>
     );
 };
 
